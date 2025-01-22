155337df6ccec37324b1cdfce3f0f7d46b61e36e
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-22 12:51:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function volume_sphere() {
-  const radiusInput = document.getElementById("radius");
-  const volumeOutput = document.getElementById("volume");
+function volume_sphere(event) {
+    //Write your code here
+	event.preventDefault();
+	const radiusInput = document.getElementById("radius");
+			const volumeOutput = document.getElementById("volume");
 
-  const radius = parseFloat(radiusInput.value);
+			const radius = parseFloat(radiusInput.value);
 
-  if (isNaN(radius) || radius < 0) {
-    volumeOutput.value = "NaN";
-  } else {
-    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-    volumeOutput.value = volume.toFixed(4);
-  }
+			if (isNaN(radius) || radius < 0) {
+				volumeOutput.value = "NaN";
+			} 
+			else {
+				const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+				volumeOutput.value = volume.toFixed(4);
+			}
 }
+window.onload =()=>{ document.getElementById('MyForm').onsubmit = volume_sphere;
+				   };
